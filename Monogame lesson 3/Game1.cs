a2d7b58b01ecc6aa68246041c9100542c2c9c41c
Gavin Anderson <362579823@GoTVDSB.ca>
2026-04-21 13:05:49 -0400

    1.0

--- Monogame lesson 3/Game1.cs
+++ Monogame lesson 3/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Monogame_lesson_3
 {
@@ -9,10 +10,12 @@ namespace Monogame_lesson_3
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Vector2 tribbleGreySpeed;
-        Rectangle window, greyTribblerect;
-        Texture2D tribbleGreyTexture;
-        SoundEffect tribbleCoo;
+        Vector2 tribbleGreySpeed, tribbleBrownSpeed, tribbleOrangeSpeed, tribbleCreamSpeed;
+        Rectangle window, greyTribblerect, tribbleBrownrect, tribbleCreamrect,tribbleOrangerect;
+        Texture2D tribbleGreyTexture, tribbleBrownTexture, tribbleCreamTexture, tribbleOrangeTexture, living_roomBackground, bedroomTexture, bathroomTexture, kitchenTexture;
+        SoundEffect tribbleCoo, openingDoor;
+        Random locate = new Random(), room = new Random();
+        int roomNumber;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -27,8 +30,14 @@ namespace Monogame_lesson_3
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.ApplyChanges();
-            greyTribblerect = new Rectangle(300, 10, 100, 100);
-            tribbleGreySpeed = new Vector2(3, 3);
+            greyTribblerect = new Rectangle(300, 100, 100, 100);
+            tribbleGreySpeed = new Vector2(3, 2);
+            tribbleBrownrect = new Rectangle(locate.Next(0, 700), locate.Next(0, 500), 100, 100);
+            tribbleBrownSpeed = new Vector2(2, 0);
+            tribbleCreamrect = new Rectangle(locate.Next(0, 700), locate.Next(0, 500), 100, 100);
+            tribbleCreamSpeed = new Vector2(3, 1);
+            tribbleOrangerect = new Rectangle(locate.Next(0, 700), locate.Next(0, 500), 100, 100);
+            tribbleOrangeSpeed = new Vector2(0, 3);
 
             base.Initialize();
         }
@@ -38,6 +47,14 @@ namespace Monogame_lesson_3
             _spriteBatch = new SpriteBatch(GraphicsDevice);
                 tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
                 tribbleCoo = Content.Load<SoundEffect>("tribble_Coo");
+            tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
+            tribbleCreamTexture = Content.Load<Texture2D>("tribbleCream");
+            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+            living_roomBackground = Content.Load<Texture2D>("living_room");
+            bathroomTexture = Content.Load<Texture2D>("bathroom");
+            bedroomTexture = Content.Load<Texture2D>("bedroom");
+            kitchenTexture = Content.Load<Texture2D>("kitchen");
+            openingDoor = Content.Load<SoundEffect>("door_open");
 
             // TODO: use this.Content to load your game content here
         }
@@ -51,6 +68,7 @@ namespace Monogame_lesson_3
             if (greyTribblerect.Left < window.Left || greyTribblerect.Right > window.Right)
             {
                 tribbleGreySpeed.X *= -1;
+
                 tribbleCoo.Play();
             }
             else if (greyTribblerect.Top < window.Top || greyTribblerect.Bottom > window.Bottom)
@@ -58,7 +76,54 @@ namespace Monogame_lesson_3
                 tribbleGreySpeed.Y *= -1;
                 tribbleCoo.Play();
             }
-           
+
+            tribbleBrownrect.X += (int)tribbleBrownSpeed.X;
+            tribbleBrownrect.Y += (int)tribbleBrownSpeed.Y;
+            if ( tribbleBrownrect.Right > window.Right - -100)
+            {
+                tribbleBrownrect.X = (-100);
+                roomNumber = room.Next(1, 4);
+                openingDoor.Play();
+                tribbleCoo.Play();
+            }
+            else if (tribbleBrownrect.Top < window.Top || tribbleBrownrect.Bottom > window.Bottom)
+            {
+                tribbleBrownSpeed.Y *= -1;
+                tribbleCoo.Play();
+            }
+            tribbleCreamrect.X += (int)tribbleCreamSpeed.X;
+            tribbleCreamrect.Y += (int)tribbleCreamSpeed.Y;
+            if (tribbleCreamrect.Left < window.Left || tribbleCreamrect.Right > window.Right)
+            {
+                tribbleCreamrect.X = locate.Next(0, 700);
+                tribbleCreamSpeed.X *= -1;
+                tribbleCoo.Play();
+            }
+            else if (tribbleCreamrect.Top < window.Top || tribbleCreamrect.Bottom > window.Bottom)
+            {
+                tribbleCreamrect.Y = locate.Next(0, 500);
+                tribbleCreamSpeed.Y *= -1;
+                tribbleCoo.Play();
+            }
+            tribbleOrangerect.X += (int)tribbleOrangeSpeed.X;
+            tribbleOrangerect.Y += (int)tribbleOrangeSpeed.Y;
+            if (tribbleOrangerect.Left < window.Left || tribbleOrangerect.Right > window.Right)
+            {
+                tribbleOrangeSpeed.X *= -1;
+                tribbleCoo.Play();
+            }
+            else if ( tribbleOrangerect.Bottom > window.Bottom - -100)
+            {
+                
+                tribbleOrangerect.Y = -100;
+                tribbleOrangeSpeed.Y++;
+                tribbleCoo.Play();
+                if (tribbleOrangeSpeed.Y > 1000)
+                {
+                    tribbleOrangeSpeed.Y = 1;
+                }
+            }
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -68,7 +133,26 @@ namespace Monogame_lesson_3
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
+            if (roomNumber == 1)
+            {
+                _spriteBatch.Draw(living_roomBackground, window, Color.White);
+            }
+            else if (roomNumber == 2)
+            {
+                _spriteBatch.Draw(bathroomTexture, window, Color.White);
+            }
+            else if (roomNumber == 3)
+            {
+                _spriteBatch.Draw(bedroomTexture, window, Color.White);
+            }
+            else
+            {
+                _spriteBatch.Draw(kitchenTexture, window, Color.White);
+            }
             _spriteBatch.Draw(tribbleGreyTexture, greyTribblerect, Color.White);
+            _spriteBatch.Draw(tribbleBrownTexture, tribbleBrownrect, Color.White);
+            _spriteBatch.Draw(tribbleCreamTexture, tribbleCreamrect, Color.White);
+            _spriteBatch.Draw(tribbleOrangeTexture, tribbleOrangerect, Color.White);
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
